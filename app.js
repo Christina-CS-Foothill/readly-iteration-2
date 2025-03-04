ebc744f8ae17c711d2cf86c575edd3b06c2d31e6
Christina Hunter <frijole16@gmail.com>
2025-03-03 17:53:13 -0800

    Added a JS file, autofilled fields in Create New Story page to get started

---
+++ app.js
@@ -0,0 +1,22 @@
+//basic JS interacting a little with the DOM. Autofilled fields in the Create New Story page.
+document.body.children[1].children[1].children[0].children[0].children[0].children[1].defaultValue =
+  "Jane Doe";
+document.body.children[1].children[1].children[0].children[0].children[1].children[1].defaultValue =
+  "The Greatest Story Ever";
+document.body.children[1].children[1].children[0].children[0].children[2].children[1].defaultValue =
+  "The Greatest Story Ever is about the greatest story ever!!!!";
+document.body.children[1].children[1].children[0].children[0].children[3].children[1].children[3].selected =
+  "selected";
+document.body.children[1].children[1].children[0].children[0].children[4].children[1].defaultValue =
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore " +
+  "magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint " +
+  "occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n" +
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore " +
+  "magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint " +
+  "occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n" +
+  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore " +
+  "magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat." +
+  "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint " +
+  "occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n";
